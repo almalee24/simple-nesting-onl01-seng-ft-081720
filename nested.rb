50aa03014d09edb8fc9ab18e494d9972ce345ea4
Alma Flores <flores.almalee@gmail.com>
2020-08-19 22:11:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,6 +86,7 @@ def adding_matz
     programmer_hash[:yukihiro_matsumoto].unshift(
           :known_for => "Ruby",
           :languages => ["LISP", "C"])
+    programmer_hash
 end
 
 def changing_alan
